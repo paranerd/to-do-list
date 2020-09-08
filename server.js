487b2b0045b6f7e467187d1dacddf58e33688443
paranerd <paranerd.development@gmail.com>
2020-09-08 09:30:50 +0200

    Changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8082
+const port = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
