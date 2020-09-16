d565943339b5ef1560d5af1d9e687abfa5dda9bc
paranerd <paranerd.development@gmail.com>
2020-09-16 12:16:16 +0200

    Code cleanup

--- controllers/item.js
+++ controllers/item.js
@@ -18,24 +18,18 @@ router.get('/', async (req, res) => {
  * Create item
  */
 router.post('/', async (req, res) => {
-	const item = await createItem(req.body);
+	try {
+		const item = await createItem(req.body);
 
-	res.json(item);
-
-	/*const name = req.body.name || "";
-	const created = req.body.created || Date.now();
-
-	const item = new Item({
-		name: name,
-		created: created,
-		modified: created
-	});
-
-	await item.save();
-
-	await notification.send(item.name + " added", req.cookies.endpoint);
-
-	res.json(item);*/
+		// Send notification
+		await notification.send(item.name + " added", req.cookies.endpoint);
+	
+		res.json(item);
+	} catch (e) {
+		console.log(e);
+		const status = e.status ? e.status : 500;
+		res.status(status).json({'error': e.message});
+	}
 });
 
 async function createItem(itemData) {
@@ -57,10 +51,38 @@ async function createItem(itemData) {
 
 	await item.save();
 
-	// await notification.send(item.name + " added", req.cookies.endpoint);
-
 	return item;
 }
+
+router.patch('/', async (req, res) => {
+	try {
+		const item = await updateItem(req.body);
+
+		// Send notification
+		await notification.send(item.name + " updated", req.cookies.endpoint);
+	
+		res.json(item);
+	} catch (e) {
+		console.log(e);
+		const status = e.status ? e.status : 500;
+		res.status(status).json({'error': e.message});
+	}
+});
+
+router.delete('/', async (req, res) => {
+	try {
+		const item = await deleteItem(req.body);
+
+		// Send notification
+		await notification.send(item.name + " removed", req.cookies.endpoint);
+
+		res.json(item);
+	} catch (e) {
+		console.log(e);
+		const status = e.status ? e.status : 500;
+		res.status(status).json({'error': e.message});
+	}
+});
 
 async function updateItem(itemData) {
 	let item = await Item.findOne({id: itemData.id});
@@ -90,7 +112,6 @@ async function updateItem(itemData) {
 }
 
 async function deleteItem(itemData) {
-	console.log(itemData);
 	const item = await Item.findOne({id: itemData.id});
 	itemData.modified = itemData.modified || Date.now();
 
@@ -111,65 +132,5 @@ async function deleteItem(itemData) {
 
 	await item.remove();
 }
-
-router.patch('/', async (req, res) => {
-	try {
-		const item = await updateItem(req.body);
-
-		// await notification.send(item.name + " updated", req.cookies.endpoint);
-	
-		res.json(item);
-	} catch (e) {
-		console.log(e);
-		const status = e.status ? e.status : 500;
-		res.status(status).json({'error': e.message});
-	}
-
-	/*let item = await Item.findOne({id: req.body.id});
-	item.name = req.body.name;
-	item.done = !!req.body.done;
-	item.modified = req.body.ts || Date.now();
-
-	await item.save();
-
-	await notification.send(item.name + " updated", req.cookies.endpoint);
-
-	res.json(item);*/
-});
-
-router.delete('/', async (req, res) => {
-	try {
-		console.log("delete", req.body);
-		const item = await deleteItem(req.body);
-
-		//await notification.send(item.name + " removed", req.cookies.endpoint);
-
-		res.json(item);
-	} catch (e) {
-		console.log(e);
-		const status = e.status ? e.status : 500;
-		res.status(status).json({'error': e.message});
-	}
-
-	/*const id = req.body.id;
-	const timestamp = req.body.ts || Date.now();
-	const item = await Item.findOne({id: id});
-
-	if (!item) {
-		res.status(404).json({});
-		return;
-	}
-
-	if (item.modified > timestamp) {
-		res.status(200).json({});
-		return;
-	}
-
-	await item.remove();
-
-	//await notification.send(item.name + " removed", req.cookies.endpoint);
-
-	res.json({});*/
-});
 
 module.exports = router;
