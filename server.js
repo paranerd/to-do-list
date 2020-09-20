4e2cef2e0699c380b6af011ff225da8fe026f3f7
paranerd <paranerd.development@gmail.com>
2020-09-20 08:34:55 +0200

    Switched to handlebars templating engine

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const db = require('./config/database');
 const notification = require('./util/notification');
 const app = express();
 const port = 8088;
+
+app.engine('.hbs', exphbs({
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    defaultLayout: 'base',
+    extname: '.hbs'
+}));
+
+// Setting template Engine
+app.set('view engine', '.hbs');
 
 app.use(cors());
 app.use(bodyParser.json());
