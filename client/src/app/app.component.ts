19d85d71dde2c8a5f2fa3f268adf06c0a7bfe7e2
paranerd <paranerd.development@gmail.com>
2020-10-13 20:41:07 +0200

    Added snackbar to app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PushService } from '@app/services/push.service';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
     selector: 'app-root',
@@ -9,11 +10,25 @@ import { PushService } from '@app/services/push.service';
 })
 export class AppComponent implements OnInit {
     title: string = 'To-Do List';
+    showSnackbar: boolean = false;
+    snackbarActionName: string = "";
+    snackbarMsg: string = "";
 
-    constructor(private push: PushService) {
+    constructor(private push: PushService, private swUpdate: SwUpdate) {
         push.init();
+
+        this.swUpdate.available.subscribe(e => {
+            // Show snackbar
+            this.snackbarMsg = "Update available!";
+            this.snackbarActionName = "Apply";
+            this.showSnackbar = true;
+        });
     }
 
     ngOnInit(): void {
+    }
+
+    applyUpdate() {
+        this.swUpdate.activateUpdate().then(() => document.location.reload());
     }
 }
