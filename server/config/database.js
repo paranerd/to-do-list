a70a0737a47b1a8938dcb476975b0c5619777bf7
paranerd <paranerd.development@gmail.com>
2020-12-01 11:32:05 +0100

    Added database connection retries

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
-// Determine environment
+const SECONDS_BETWEEN_RETRIES = 5;
 const server = process.env.DOCKER ? 'mongo' : '127.0.0.1';
+const connectionString = `mongodb://${server}:27018/todo`;
+const connectionParams = {
+    useNewUrlParser: true,
+    //useUnifiedTopology: true, --> causes timeout error, so don't use for now
+    useCreateIndex: true
+};
 
-mongoose.connect(`mongodb://${server}:27017/todo`, {
-	useNewUrlParser: true,
-	//useUnifiedTopology: true, --> causes timeout error, so don't use for now
-	useCreateIndex: true
-});
+function connectWithRetry() {
+    return mongoose.connect(connectionString, connectionParams, function(err) {
+        if (err) {
+            console.error(`Failed to connect to mongo on startup - retrying in ${SECONDS_BETWEEN_RETRIES} sec`, err);
+            setTimeout(connectWithRetry, SECONDS_BETWEEN_RETRIES * 1000);
+        }
+    });
+};
+
+connectWithRetry();
