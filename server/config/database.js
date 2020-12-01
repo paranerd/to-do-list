d1993a909be92e7a0ebd8923eac0091325bdfe8b
paranerd <paranerd.development@gmail.com>
2020-12-01 11:41:23 +0100

    Fixed MongoDB port

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const SECONDS_BETWEEN_RETRIES = 5;
 const server = process.env.DOCKER ? 'mongo' : '127.0.0.1';
-const connectionString = `mongodb://${server}:27018/todo`;
+const connectionString = `mongodb://${server}:27017/todo`;
 const connectionParams = {
     useNewUrlParser: true,
     //useUnifiedTopology: true, --> causes timeout error, so don't use for now
